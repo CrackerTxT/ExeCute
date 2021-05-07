e4dda513a4de80582117bb198055a49715efcba0
Paulo J Amancio <32086577+CrackerTxT@users.noreply.github.com>
2021-05-07 04:40:23 -0300

    Variáveis de matematica
    Variáveis de inicio para cálculos simples

---
+++ v_math.py
@@ -0,0 +1,25 @@
+# Soma
+def som(a, b):
+    s = a + b
+    return s
+
+# Multiplicação
+def mut(a, b):
+    m = a * b
+    return m
+
+# Subtração
+def sub(a, b):
+    s1 = a - b
+    return s1
+
+# Potencia
+def pot(a, b):
+    p = a ** b
+    return p
+
+# Resto
+def res(a, b):
+    r = a / b
+    return r
+
